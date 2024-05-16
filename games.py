fe95b7856a4fc419852fd4ed718d6e44796154a0
pwschenkenberg <pwschenkenberg@gmail.com>
2024-05-16 15:00:17 -0500

    Update games.py

--- games.py
+++ games.py
@@ -1,8 +1,11 @@
 # this will be the code to create, store, and manage games
 
-# Create Games class
-class Games:
+# Create Game class
+class Game:
   def __init__(self,home_team,away_team):
     self.home_team = home_team
     self.away_team = away_team
+
+  def __str__(self):
+    return f"{self.home_team} vs {self.away_team}"
   
