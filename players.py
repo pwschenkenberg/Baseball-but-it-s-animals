5903c3847dc42dc4a6aa2e46211e0c39138a422a
pwschenkenberg <pwschenkenberg@gmail.com>
2024-05-16 14:27:31 -0500

    Update players.py

--- players.py
+++ players.py
@@ -7,3 +7,15 @@ class Player:
 
   def __str__(self):
     return f"{self.name}"
+
+  # Player Characteristics
+  animal = 'none'
+  height = 0
+  weight = 0
+  handedness = 'none'
+  topSpeed = 0
+  acceleration = 0
+  strength = 0
+
+  # Stats - Dictionary (maybe should be list?) that is added to after every game
+  statistics = {}
