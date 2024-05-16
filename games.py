757060d5e7eefa451af62a047c605bf6df669b5e
pwschenkenberg <pwschenkenberg@gmail.com>
2024-05-16 14:55:49 -0500

    Update games.py

--- games.py
+++ games.py
@@ -1,1 +1,8 @@
-# File to create and manage games
+# this will be the code to create, store, and manage games
+
+# Create Games class
+class Games:
+  def __init__(self,home_team,away_team):
+    self.home_team = home_team
+    self.away_team = away_team
+  
