091eb86c5f5bf2df42b91763094feefbe38641af
pwschenkenberg <pwschenkenberg@gmail.com>
2024-06-12 11:23:00 -0500

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,5 +1,14 @@
+# scaling factors to convert feet to pixels
 PX_TO_FT_STRIKE_ZONE = 100
 PX_TO_FT_FIELD = 2
 
+# dimensions in feet for game components
 STRIKE_ZONE_HEIGHT_FT = 2.5
 STRIKE_ZONE_WIDTH_FT = 1.5
+
+# colors
+FIELD_GRASS = (70,214,46)
+FIELD_DIRT = (228,201,86)
+FIELD_LINES = (255,255,255)
+
+BACKGROUND = (129,49,17)
